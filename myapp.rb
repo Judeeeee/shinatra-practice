b103668f294f95ebe0148205cd41ef324082f13e
Judeeeee <miku.nezasa@wiley-corp.com>
2022-04-08 17:10:11 +0900

    クラス変数が扱いにくいので、定義したクラスをバラして、メソッドのみ残した

--- myapp.rb
+++ myapp.rb
@@ -9,29 +9,25 @@ require 'erb'
 include ERB::Util
 memos = {}
 
-class DataBaseHandles
-  class << self
-    def load
-      @memos = JSON.parse(File.read('json/memo.json'), symbolize_names: true)
-    end
+def load
+  @memos = JSON.parse(File.read('json/memo.json'), symbolize_names: true)
+  end
 
-    def store
-      File.open('json/memo.json', 'w') do |file|
-        JSON.dump(@memos, file)
-      end
-    end
-
-    def add(memo)
-      @id = SecureRandom.uuid
-      @memos[@id] = memo
-    end
+def store
+  File.open('json/memo.json', 'w') do |file|
+   JSON.dump(@memos, file)
   end
 end
 
+def add(memo)
+  @id = SecureRandom.uuid
+  @memos[@id] = memo
+end
+
+
 # top
 get '/memos' do
-  DataBaseHandles.load
-  @memolist = DataBaseHandles.load.map{ |memo| "<a href=/memos/#{memo[0]}>#{html_escape(memo[1][:title])}</a>" }.join("<br>")
+  @memolist = load.map{ |memo| "<a href=/memos/#{memo[0]}>#{html_escape(memo[1][:title])}</a>" }.join("<br>")
   erb :index
 end
 
@@ -42,8 +38,8 @@ end
 
 post '/memos' do
   @memo = { title: params[:memo_title].to_s, text: params[:memo_text].to_s }
-  DataBaseHandles.add(@memo)
-  DataBaseHandles.store
+  add(@memo)
+  store
 
   redirect '/memos'
 end
@@ -51,7 +47,7 @@ end
 # show
 get '/memos/:id' do
   @memo_id = params[:id].to_sym
-  @memo = DataBaseHandles.load[@memo_id]
+  @memo = load[@memo_id]
   @title = @memo[:title]
   @text = @memo[:text]
 
@@ -61,15 +57,15 @@ end
 # 削除
 delete '/memos/:id' do
   @memo_id = params[:id].to_sym
-  DataBaseHandles.load.delete(@memo_id)
-  DataBaseHandles.store
+  load.delete(@memo_id)
+  store
 
   redirect '/memos'
 end
 
 get '/memos/:id/edit' do
   @memo_id = params[:id].to_sym
-  @memo = DataBaseHandles.load[@memo_id]
+  @memo = load[@memo_id]
   @title = @memo[:title]
   @text = @memo[:text]
 
@@ -78,7 +74,7 @@ end
 
 # edit
 post '/memos/:id/edit' do
-  DataBaseHandles.load
+  load
 
   erb :edit
 end
@@ -87,8 +83,8 @@ end
 patch '/memos/:id' do
   @memo_id = params[:id].to_sym
   @memo = { title: params[:memo_title].to_s, text: params[:memo_text].to_s }
-  DataBaseHandles.load[@memo_id] = @memo
-  DataBaseHandles.store
+  load[@memo_id] = @memo
+  store
 
   erb :edit
   redirect '/memos'
