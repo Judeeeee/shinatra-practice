4e56375b0f1ff1115f9185da769a23414e46f91e
Judeeeee <miku.nezasa@wiley-corp.com>
2022-04-27 14:56:34 +0900

    プレースホルダーを用いたSQL操作がしたかったので、一旦定義していたメソッドを消してブラウザ上で動作するかの確認を行った

--- myapp.rb
+++ myapp.rb
@@ -10,48 +10,10 @@ require 'pg'
 
 include ERB::Util
 
-def postgresql(sql)
-  conn = PG.connect( dbname: 'shinatra_memoapp' )
-  memos_data = conn.exec(sql)
-  conn.finish
-  memos_data
-end
-
-def select
-  sql = "SELECT * FROM memodata"
-  postgresql(sql)
-end
-
-def create(id)
-  title = params[:memo_title]
-  text = params[:memo_text]
-  sql = "INSERT INTO memodata values ('#{id}', '#{title}', '#{text}')"
-  postgresql(sql)
-end
-
-def find(id)
-  sql = "SELECT * FROM memodata WHERE id = '#{id}'"
-  memo = postgresql(sql)
-  memo[0]
-end
-
-def update
-  title = params[:memo_title]
-  text = params[:memo_text]
-  id = params[:id]
-  sql = "UPDATE memodata SET title = '#{title}', sentence = '#{text}' WHERE id = '#{id}'"
-  postgresql(sql)
-end
-
-def delete
-  id = params[:id].to_sym
-  sql = "DELETE FROM memodata WHERE id = '#{id}'"
-  postgresql(sql)
-end
-
 
 get '/memos' do
-  memos = select
+  conn = PG.connect( dbname: 'shinatra_memoapp' )
+  memos = conn.exec('SELECT * FROM memodata')
   @memo_list = []
   memos.map { |memo| @memo_list << memo }
   erb :index
@@ -64,35 +26,48 @@ end
 
 post '/memos' do
   id = SecureRandom.uuid.to_s
-  create(id)
+  title = params[:memo_title]
+  text = params[:memo_text]
+  conn = PG.connect( dbname: 'shinatra_memoapp' )
+  conn.exec_params('INSERT INTO memodata (id, title, sentence) VALUES ($1, $2, $3)', [id, title, text])
+
   redirect '/memos'
 end
 
 
 get '/memos/:id' do
   @memo_id = params[:id].to_sym
-  @memo = find(@memo_id)
+  conn = PG.connect( dbname: 'shinatra_memoapp' )
+  @memo = conn.exec_params('SELECT * FROM memodata WHERE id = $1', [@memo_id])
+
   erb :show
 end
 
 
 delete '/memos/:id' do
-  delete
+  @memo_id = params[:id].to_sym
+  conn = PG.connect( dbname: 'shinatra_memoapp' )
+  conn.exec_params('DELETE FROM memodata WHERE id = $1', [@memo_id])
   redirect '/memos'
 end
 
 
 get '/memos/:id/edit' do
   @memo_id = params[:id].to_sym
-  @memo = find(@memo_id)
+  conn = PG.connect( dbname: 'shinatra_memoapp' )
+  @memo = conn.exec_params('SELECT * FROM memodata WHERE id = $1', [@memo_id])
   erb :edit
 end
 
 
 patch '/memos/:id' do
   @memo_id = params[:id].to_sym
-  @memo = find(@memo_id)
-  update
+  conn = PG.connect( dbname: 'shinatra_memoapp' )
+  @memo = conn.exec_params('SELECT * FROM memodata WHERE id = $1', [@memo_id])
+  title = params[:memo_title]
+  text = params[:memo_text]
+  id = params[:id]
+  conn.exec_params('UPDATE memodata SET title = $1, sentence = $2 WHERE id = $3',[title, text,id])
   erb :edit
   redirect '/memos'
 end
